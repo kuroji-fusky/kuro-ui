c78cd7d3c1094677d2d1d5b74966bc6b6f029014
Kerby Keith Aquino <hello@kurojifusky.com>
2024-08-11 20:32:20 +0800

    add autoprefixer and unplugin-icons

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config"
-
 import vercel from "@astrojs/vercel/serverless"
 import tailwind from "@astrojs/tailwind"
 import svelte from "@astrojs/svelte"
+import autoprefixer from "autoprefixer"
+import Icons from "unplugin-icons/vite"
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,16 @@ export default defineConfig({
   integrations: [tailwind(), svelte()],
   prefetch: {
     prefetchAll: true,
+  },
+  vite: {
+    plugins: [
+      Icons({
+        compiler: "astro",
+        customCollections: {},
+      }),
+    ],
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
   },
 })
