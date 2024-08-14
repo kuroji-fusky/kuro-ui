d2c6492a0cec95b8203233beb014d19175977b3b
Kerby Keith Aquino <hello@kurojifusky.com>
2024-08-15 06:30:21 +0800

    add svelte

---
+++ packages/@kuro-ui-svelte/src/components/split-panel/SplitPanel.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let windowBreakpoint = "768px"
+  export let containerBreakpoint = ""
+  export let shelfMinWidth = "300px"
+</script>
+
+<div>
+  <div>
+    <slot name="shelf" />
+  </div>
+
+  <div>
+    <slot />
+  </div>
+</div>
---
+++ packages/@kuro-ui-svelte/src/index.ts
@@ -0,0 +1,1 @@
+export * from "./components/split-panel/SplitPanel.svelte"
